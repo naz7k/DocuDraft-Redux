9833f0eb5e0cc5d50217dd9ea62bbe19a96d9185
naz7k <nazar.kolassa@gmail.com>
2024-04-18 20:31:28 -0400

    table handling

--- replacer.py
+++ replacer.py
@@ -11,4 +11,10 @@ def replace(template: Template, replace_data: ReplaceData, output_dir: str):
                 run_text = run.text.replace(replace_data.get_key().get_code() + wildcard, replace_data.get_data()[wildcard])
                 run.text = run_text
 
-
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for run in cell.runs:
+                    for wildcard in replace_data.get_data().keys():
+                        run_text = cell.text.replace(replace_data.get_key().get_code() + wildcard, replace_data.get_data()[wildcard])
+                        run.text = run_text
