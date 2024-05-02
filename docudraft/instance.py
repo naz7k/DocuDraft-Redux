16f82e58f82b74a4d14bcddbd43df4563da1b2f2
naz7k <nazar.kolassa@gmail.com>
2024-05-01 22:13:32 -0400

    instance & cli functioning with save-load .dxtp system

--- docudraft/instance.py
+++ docudraft/instance.py
@@ -2,11 +2,12 @@ import docudraft.loader as loader
 import docudraft.replace as replace
 import docudraft.defaults as defaults
 from docudraft.template.template_package import TemplatePackage
+from docudraft.user.data.wordmap import WordMap
 
 
 class Instance:
     loaded_template_package: TemplatePackage | None
-    template_package_dir: str   # where to look for dxtp files, used for gui
+    template_package_dir: str  # where to look for dxtp files, used for gui
     output_dir: str
 
     def __init__(self):
@@ -22,9 +23,20 @@ class Instance:
         Runs the program, outputs drafted docs.
         :return: True if operation is successful. Otherwise, returns false.
         """
-        if self.loaded_template_package is None:
+        try:
+            for template in self.loaded_template_package.template_package:
+                if replace.search_and_replace(template, self.loaded_template_package.word_map,
+                                              self.loaded_template_package.key, self.output_dir):
+                    return True
+
+        except AttributeError:
             return False
 
-        for template in self.loaded_template_package.template_package:
-            if replace.search_and_replace(template, self.loaded_template_package.word_map, self.loaded_template_package.key, self.output_dir):
-                return True
+    def modify_word_map(self, **kwargs: str) -> WordMap:
+        try:
+            for i in kwargs:
+                if i in self.loaded_template_package.word_map.word_map:
+                    self.loaded_template_package.word_map.word_map[i] = kwargs[i]
+            return self.loaded_template_package.word_map
+        except AttributeError:
+            return None
